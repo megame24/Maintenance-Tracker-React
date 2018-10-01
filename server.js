b14746f004d961e73cf670542f1af7c064ecc380
Innocent Ngene <ngeneinnocent@gmail.com>
2018-10-01 09:27:30 +0100

    chore(deployment) deploy to heroku - create a server for the front-end - create a heroku app [Delivers #160883573]

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.static(path.join(__dirname, 'dist')));
+
+const PORT = process.env.PORT || 3000;
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.all('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+app.listen(PORT, () => {
+  console.log(`server started on port: ${PORT}`);
+});
